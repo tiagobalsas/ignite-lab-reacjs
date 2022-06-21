624274555986b9efd98f8b3f8c10e0f94ff27966
Tiago Santos <tiago881@gmail.com>
2022-06-21 12:23:41 -0300

    feat:add request API

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,5 +1,31 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
-  return <h1 className='font-bold'>Hello World</h1>;
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
+  return (
+    <>
+      <ul>
+        {data?.lessons.map((lesson) => {
+          return <li key={lesson.id}>{lesson.title}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
