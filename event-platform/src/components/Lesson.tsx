5bf902f7fc72693a20d3b3b8d31df542da2b285e
Tiago Santos <tiago881@gmail.com>
2022-06-22 13:45:29 -0300

    feat:add date & time

--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -1,4 +1,6 @@
 import { CheckCircle, Lock } from 'phosphor-react';
+import { format, isPast } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 interface LessonProps {
   title: string;
@@ -6,12 +8,19 @@ interface LessonProps {
   availableAt: Date;
   type: 'live' | 'class';
 }
-
+/* ' • ' */
 export function Lesson(props: LessonProps) {
-  const isLessonAvailable = false;
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormated = format(
+    props.availableAt,
+    "EEEE' • 'd' de 'MMMM' • 'k'h'",
+    {
+      locale: ptBR,
+    }
+  );
   return (
     <a href=''>
-      <span className='text-gray-300'>{props.availableAt.toDateString()}</span>
+      <span className='text-gray-300'>{availableDateFormated}</span>
 
       <div className='rounded border border-gray-500 p-4 mt-2'>
         <header className='flex items-center justify-between'>
