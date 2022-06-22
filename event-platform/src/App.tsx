899c22c10ace139b54f7d7a7425b76ae16639796
Tiago Santos <tiago881@gmail.com>
2022-06-22 10:07:09 -0300

    feat(import):add Evento component

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,31 +1,9 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Evento } from './pages/Evento';
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
-    <>
-      <ul>
-        {data?.lessons.map((lesson) => {
-          return <li key={lesson.id}>{lesson.title}</li>;
-        })}
-      </ul>
-    </>
-  );
+    <Evento />
+  )
 }
 
 export default App;
