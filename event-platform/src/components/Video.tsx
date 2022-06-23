c97eca08fd85bc2d6cf2543d9b97f9ea58231492
Tiago Santos <tiago881@gmail.com>
2022-06-23 10:09:22 -0300

    feat(card):add wallpapers exclusive

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -78,7 +78,25 @@ export function Video() {
               <CaretRight size={24} />
             </div>
           </a>
-          <a href=''></a>
+
+          <a
+            href=''
+            className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'
+          >
+            <div className='bg-green-700 h-full p-6 flex items-center'>
+              <FileArrowDown size={40} />
+            </div>
+            <div className='py-6 leading-relaxed'>
+              <strong className='text-2xl'>wallpapers exclusivos</strong>
+              <p className='text-sm text-gray-200 mt-2'>
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
+              </p>
+            </div>
+            <div className='h-full p-6 flex items-center'>
+              <CaretRight size={24} />
+            </div>
+          </a>
         </div>
       </div>
     </div>
