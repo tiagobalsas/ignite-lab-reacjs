31c2bc4cbb397881c82c8281cec55e5b4b6103c8
Tiago Santos <tiago881@gmail.com>
2022-06-23 16:39:42 -0300

    change:add Evento to Event component

--- event-platform/src/Router.tsx
+++ event-platform/src/Router.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { Evento } from './pages/Evento';
+import { Event } from './pages/Event';
 
 export function Router() {
   return (
     <Routes>
       <Route path='/' element={<h1>Home</h1>} />
-      <Route path='/event' element={<Evento />} />
+      <Route path='/event' element={<Event />} />
     </Routes>
   );
 }
--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -2,7 +2,7 @@ import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 import { Video } from '../components/Video';
 
-export function Evento() {
+export function Event() {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
