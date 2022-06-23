46099144033b8b70cb9623d4f8dfa9bdab2be882
Tiago Santos <tiago881@gmail.com>
2022-06-23 10:04:00 -0300

    feat(card):add additional material

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,4 +1,9 @@
-import { DiscordLogo, Lightning } from 'phosphor-react';
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from 'phosphor-react';
 
 export function Video() {
   return (
@@ -54,7 +59,27 @@ export function Video() {
             </a>
           </div>
         </div>
-        <div></div>
+        <div className='gap-8 mt-20 grid grid-cols-2'>
+          <a
+            href=''
+            className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'
+          >
+            <div className='bg-green-700 h-full p-6 flex items-center'>
+              <FileArrowDown size={40} />
+            </div>
+            <div className='py-6 leading-relaxed'>
+              <strong className='text-2xl'>Material Complementar</strong>
+              <p className='text-sm text-gray-200 mt-2'>
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
+              </p>
+            </div>
+            <div className='h-full p-6 flex items-center'>
+              <CaretRight size={24} />
+            </div>
+          </a>
+          <a href=''></a>
+        </div>
       </div>
     </div>
   );
