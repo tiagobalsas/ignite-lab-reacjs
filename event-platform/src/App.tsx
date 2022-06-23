5e9d6387ca792698bbbd346b5bd4619349874ff7
Tiago Santos <tiago881@gmail.com>
2022-06-23 11:11:13 -0300

    change:Directory

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,9 +1,17 @@
-import { Evento } from './pages/Evento';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import { client } from './lib/apollo';
+import { Router } from './Router';
 
 function App() {
   return (
-    <Evento />
-  )
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
