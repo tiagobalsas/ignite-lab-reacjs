5d702baea909e079fd8562dd875bbc79a8ebfca9
Tiago Santos <tiago881@gmail.com>
2022-06-23 11:08:53 -0300

    feat:add Router component

---
+++ event-platform/src/Router.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+import { Evento } from './pages/Evento';
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/evento' element={<Evento />} />
+    </Routes>
+  );
+}
